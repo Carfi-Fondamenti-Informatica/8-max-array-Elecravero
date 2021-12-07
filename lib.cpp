f22dc668680531d73c1147e16f0ee3d190da14f8
Elecravero <91529865+Elecravero@users.noreply.github.com>
2021-12-07 13:38:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+float funzione (const int dim, float a[]){
+       a[dim];
+       float max;
+       max= a[0];
+       for(int i=0; i<dim; i++){
+           if(a[i]>max){
+               max=a[i];
+           }else if(a[i]==max){
+           }
+       }
+       return max;
+   }
