fad464ed7575803a59bd1756358aaa31ef1dd7bb
Elecravero <91529865+Elecravero@users.noreply.github.com>
2021-12-07 13:40:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int dim=0;
+        //cout<<"inserisci dimensione"<<endl;
+        cin>>dim;
+        float a[dim];
+        for (int i=0; i<dim;i++) {
+            //cout << "inserisci valori" << endl;
+            cin >> a[i];
+        }
+        //cout<<"il massimo è"<<endl;
+        cout<<funzione(dim, a)<<endl;
+
   return 0;
 }
